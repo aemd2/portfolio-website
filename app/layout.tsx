ea28aed66c314d346803ac80f74e37d903d93a6a
Emil Donchev <aemd2donchev@gmail.com>
2025-04-17 09:10:42 +0300

    Update layout to use new e-favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,8 +6,8 @@ export const metadata: Metadata = {
   description: 'Product Designer based in Plovdiv, Bulgaria | Creating user-centric digital experiences',
   generator: 'v0.dev',
   icons: {
-    icon: '/favicon.ico',
-    apple: '/apple-icon.png',
+    icon: '/e-favicon.ico',
+    apple: '/e-logo.svg',
   },
 }
 
